901526f854bd3e8fe12a3e84342b3f57d706547a
mustafa-sab3alarab <mustafa777alarab@gmail.com>
2023-07-01 22:51:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cinematickets/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.cinematickets
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.example.cinematickets.screens.BookingScreen
+import com.example.cinematickets.ui.theme.CinemaTicketsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        setContent {
+            CinemaTicketsTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    BookingScreen()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cinematickets/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.cinematickets.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Orange80 = Color(0xFFFF5622)
+val OrangeGrey80 = Color(0xFFB87866)
+val DarkGrayBrown = Color(0xFF968E85)
+
+val Orange40 = Color(0xFF7A412F)
+val OrangeGrey40 = Color(0xFF6C4640)
+val DarkGrayBrown40 = Color(0xFF5D5953)
+
+
+val LightWhite = Color(0x4DFFFFFF)
+val Gray = Color(0xFF858E90)
+val TextWhite = Color(0xFFEAF0F3)
